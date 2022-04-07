389c540ca1d45e43f2802e89188c67ae8c8ff395
usama.saeed <usama.saeed@astera.com>
2022-04-07 23:17:25 +0500

    initial commit - solution setup

---
+++ FibonacciTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace FibonacciTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
