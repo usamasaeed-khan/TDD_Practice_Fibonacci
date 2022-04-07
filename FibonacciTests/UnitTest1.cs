38baa1a56218d97e01f9d2fc3c2998513080ed91
usama.saeed <usama.saeed@astera.com>
2022-04-08 00:00:36 +0500

    add new test case - failed

--- FibonacciTests/UnitTest1.cs
+++ FibonacciTests/UnitTest1.cs
@@ -13,6 +13,7 @@ public class Tests
     [TestCase(0, 0)]
     [TestCase(1, 1)]
     [TestCase(1, 2)]
+    [TestCase(2, 3)]
     public void TestFibonacci(int expected, int index)
     {
         Assert.AreEqual(expected, GetFibonacci(index));
